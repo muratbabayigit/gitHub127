f503439bf6b523a90f7afb3e17315e434cd60f38
Murat Babayiğit <babayigit@gmail.com>
2023-09-28 12:43:06 +0300

    ikinci gun

---
+++ src/IkinciGun1.java
@@ -0,0 +1,5 @@
+public class IkinciGun1 {
+    public static void main(String[] args) {
+        System.out.println("Yeni bir işlem yapıyorum");
+    }
+}
--- src/ilkGitDosyasi.java
+++ src/ilkGitDosyasi.java
@@ -1,5 +1,6 @@
 public class ilkGitDosyasi {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("Push Kısayol");
     }
 }
